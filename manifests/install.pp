83e5a9ebdc1807f7a3fd60a0e4b0ec19e31a17fa
Pierig Le Saux <lesaux@pythian.com>
2015-01-06 15:32:21 -0500

    adding param to install or not install haproxy, might be managed by another module

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,9 +1,11 @@
 class redishappy::install {
 
   if $redishappy::haproxy {
-    ensure_packages( 'haproxy' )
     package {'redishappy-haproxy':
       ensure   => present,
+    }
+    if $redishappy::haproxy_install {
+      ensure_packages( 'haproxy' )
     }
   } else {
     package {'redishappy-haproxy':
